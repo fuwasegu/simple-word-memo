c1dc9e2fdc389383cdbe05241a18cc6b25978d26
lunain84 <h_takeshita@yumemi.co.jp>
2022-02-22 03:00:30 +0900

    refactor: :bulb:  テーブル再定義

---
+++ database/migrations/1970_01_01_000000_create_users_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->index();
+            $table->string('email')->index()->unique();
+            $table->string('google_id')->unique();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+}
